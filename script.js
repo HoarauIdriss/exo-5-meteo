3e2cb12ac07e7bd8f3ff6386057ac8a569733cbd
ILLAOI <Idriss.HOARAU@laposte.net>
2018-06-20 08:58:02 +0400

    Exo 5 terminer

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 $(function (){
     // console.log($)
     $.ajax({
-        url : 'http://localhost:3208/meteo/Reunion',
+        url : 'http://localhost:3222/meteo/Reunion',
         type : 'GET',
         dataType: 'json' ,
         success: function(data) {
             var  donnees = JSON.parse(data.body)
-            $('#r1').append('La température du jour à la Réunion:' +' '+ donnees.main.temp +'</br>')
+            $('#r1').append('La température du jour à la Réunion:' +' '+ donnees.main.temp +'°C'+'</br>')
             // console.log(donnees);
             console.log(data.body);            // $('.met').append('<div> </div>')
       
@@ -19,12 +19,12 @@ $(function (){
     })
 
     $.ajax({
-        url : 'http://localhost:3208/meteo/Paris',
+        url : 'http://localhost:3222/meteo/Paris',
         type : 'GET',
         dataType: 'json' ,
         success: function(data) {
             var  donnees = JSON.parse(data.body)
-            $('#r2').append('La température du jour à Paris:' +' '+ donnees.main.temp +'</br>')
+            $('#r2').append('La température du jour à Paris:' +' '+ donnees.main.temp + '°C' +'</br>')
             // console.log(donnees);
             console.log(data.body);            // $('.met').append('<div> </div>')
       
@@ -36,12 +36,12 @@ $(function (){
         
     })
     $.ajax({
-        url : 'http://localhost:3208/meteo/Marseille',
+        url : 'http://localhost:3222/meteo/Marseille',
         type : 'GET',
         dataType: 'json' ,
         success: function(data) {
             var  donnees = JSON.parse(data.body)
-            $('#r3').append('La température du jour à Marseille :' +' '+ donnees.main.temp +'</br>')
+            $('#r3').append('La température du jour à Marseille :' +' '+ donnees.main.temp +'°C'+'</br>')
             // console.log(donnees);
             console.log(data.body);            // $('.met').append('<div> </div>')
       
@@ -54,12 +54,12 @@ $(function (){
     })
 
     $.ajax({
-        url : 'http://localhost:3208/meteo/Moscou',
+        url : 'http://localhost:3222/meteo/Moscou',
         type : 'GET',
         dataType: 'json' ,
         success: function(data) {
             var  donnees = JSON.parse(data.body)
-            $('#r4').append('La température du jour à Moscou:' +' '+ donnees.main.temp +'</br>')
+            $('#r4').append('La température du jour à Moscou:' +' '+ donnees.main.temp +'°C'+'</br>')
             // console.log(donnees);
             console.log(data.body);            // $('.met').append('<div> </div>')
       
--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-var express = require ('express');
+var express = require('express');
 var server = express();
 var request = require('request');
 
 server.use(express.static(__dirname));
 
 
-server.get("/",function(req,res){
-    res.sendFile(__dirname +"/index.html");
+server.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
 });
 
-server.get("/meteo/Reunion",function(req,res){
-    const request = require('request');
+server.get("/meteo/Reunion", function (req, res) {
+   
     let apiKey = 'c972e85bfe843d82722b1cce870768aa';
     let city = 'Reunion';
-    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`    
+    let url = `http://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${apiKey}`
     request(url, function (err, response, body) {
         if (err) {
             console.log('error:', error);
@@ -26,11 +26,43 @@ server.get("/meteo/Reunion",function(req,res){
 });
 
 
-server.get("/meteo/Paris",function(req,res){
-    const request = require('request');
+server.get("/meteo/Paris", function (req, res) {
+   
     let apiKey = 'c972e85bfe843d82722b1cce870768aa';
     let city = 'Paris';
-    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`    
+    let url = `http://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${apiKey}`
+    request(url, function (err, response, body) {
+        if (err) {
+            console.log('error:', error);
+        } else {
+            // var weather = JSON.parse(body)
+            console.log('body:', body);
+        }
+        res.send(response)
+    });
+})
+
+server.get("/meteo/Marseille", function (req, res) {
+    
+    let apiKey = 'c972e85bfe843d82722b1cce870768aa';
+    let city = 'Marseille';
+    let url = `http://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${apiKey}`
+    request(url, function (err, response, body) {
+        if (err) {
+            console.log('error:', error);
+        } else {
+            // var weather = JSON.parse(body)
+            console.log('body:', body);
+        }
+        res.send(response)
+    });
+})
+
+server.get("/meteo/Moscou", function (req, res) {
+    
+    let apiKey = 'c972e85bfe843d82722b1cce870768aa';
+    let city = 'Moscou';
+    let url = `http://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${apiKey}`
     request(url, function (err, response, body) {
         if (err) {
             console.log('error:', error);
@@ -41,42 +73,9 @@ server.get("/meteo/Paris",function(req,res){
         res.send(response)
     });
 
-    server.get("/meteo/Marseille",function(req,res){
-        const request = require('request');
-        let apiKey = 'c972e85bfe843d82722b1cce870768aa';
-        let city = 'Marseille';
-        let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`    
-        request(url, function (err, response, body) {
-            if (err) {
-                console.log('error:', error);
-            } else {
-                // var weather = JSON.parse(body)
-                console.log('body:', body);
-            }
-            res.send(response)
-        });
-
-        server.get("/meteo/Moscou",function(req,res){
-            const request = require('request');
-            let apiKey = 'c972e85bfe843d82722b1cce870768aa';
-            let city = 'Moscou';
-            let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`    
-            request(url, function (err, response, body) {
-                if (err) {
-                    console.log('error:', error);
-                } else {
-                    // var weather = JSON.parse(body)
-                    console.log('body:', body);
-                }
-                res.send(response)
-            });
-
 });
 
-server.listen(3208);})})
-
-
-
+server.listen(3222);
 // http://api.openweathermap.org/data/2.5/weather?q=Reunion&APPID=c972e85bfe843d82722b1cce870768aa   // Reunion
 
 // http://api.openweathermap.org/data/2.5/weather?q=Paris&APPID=c972e85bfe843d82722b1cce870768aa    // Paris
